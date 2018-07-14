f9d9cc9bf7c333591937d0a49ecd6bd1513906f2
ranggarizky <ranggarizky@bitbucket.com>
2018-07-14 14:25:12 +0700

    PreparedDeploy

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const members = require("./routes/api/members");
 const posts = require("./routes/api/posts");
 const discussions = require("./routes/api/discussions");
 const passport = require("passport");
+const path = require('path');
 
 mongoose
     .connect(db)
@@ -34,6 +35,16 @@ app.use('/api/projects',projects);
 app.use('/api/discussions',discussions);
 app.use('/api/members',members);
 app.use('/api/notifications',notifications);
+
+//static asset saat produksi
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static('client/build'));
+
+    app.get('*',(req,res) => {
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    });
+}
 
 const port = process.env.PORT || 5000;
 
